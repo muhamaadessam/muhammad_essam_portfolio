06f02932a8b7e312266735130b4b357668df3984
muhammadessam <muhammad159e@gmail.com>
2025-05-22 21:31:54 +0300

    Update Portfolio

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1246582488"
+    serviceWorkerVersion: "2737481728"
   }
 });
--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "966112854fe9281012936608c278362d",
+const RESOURCES = {"flutter_bootstrap.js": "ffa37a89136975d3a8d00b8daf06eba5",
 "version.json": "25d035d8a90afa8391206d83e3cd7ba8",
 "index.html": "3b6bff0c5cb35db90106fab16266a89e",
 "/": "3b6bff0c5cb35db90106fab16266a89e",
